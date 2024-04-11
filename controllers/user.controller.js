f6050dc8209e54db041cd0750ebedbb4055a6f9a
Abdulloh PC <sabirovabdulloh2004@gmail.com>
2024-04-11 07:41:58 +0500

    pol

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -143,7 +143,7 @@ async function getMe(req, res) {
       return familyMembers.push({ member: (item.userId1 === user.id ? item.userId2 : item.userId1), relation: item.relationship })
     })
 
-    res.status(200).json({ ...userData.dataValues, userRole, anthropometry: anthropometryData, family: familyMembers });
+    res.status(200).json({ ...userData.dataValues, roles: userRole, anthropometry: anthropometryData, family: familyMembers });
 
   } catch (error) { }
 }
